fbd24fbfa37a0b30ae985e4fade596ab0c8e4d18
rasoolnekouei <67897832+rasoolnekouei@users.noreply.github.com>
2023-07-23 18:40:40 +0330

    Delete some comments

--- Attendance device.ino
+++ Attendance device.ino
@@ -193,32 +193,5 @@ void loop()
  ding();
  
  
-  //Show UID on serial monitor
- /* Serial.println();
-  Serial.print(" UID tag :");
-  String content= "";
-  byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
-  {
-     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-     Serial.print(mfrc522.uid.uidByte[i], HEX);
-     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     content.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-  content.toUpperCase();
-  Serial.println();
-  if (content.substring(1) == "8E 39 32 50") //change UID of the card that you want to give access
-  {
-    Serial.println(" Access Granted ");
-    Serial.println(" Welcome Mr.Circuit ");
-    delay(1000);
-    Serial.println(" Have FUN ");
-    Serial.println();
-    statuss = 1;
-  }
-  
-  else   {
-    Serial.println(" Access Denied ");
-    delay(3000);
-  }*/
+
 } 
